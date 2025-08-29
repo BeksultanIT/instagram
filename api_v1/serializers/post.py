e649da0af61c7f839267f779abc4a10bd0049f6a
Beksultan <sharshenbekov.tech@gmail.com>
2025-08-29 16:51:35 +0600

    сделал права доступа добавил проверку можно редактировать только свои

--- api_v1/serializers/post.py
+++ api_v1/serializers/post.py
@@ -3,7 +3,29 @@ from webapp.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    image_url = serializers.SerializerMethodField(read_only=True)
+    likes_count = serializers.IntegerField(source='like_users.count', read_only=True)
+    is_liked = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Post
-        fields = ['id','image', 'content', 'author']
-        read_only_fields = ['id']
+        fields = ['id', 'image', 'image_url', 'content', 'author', 'likes_count', 'is_liked']
+        read_only_fields = ['id', 'author', 'likes_count', 'is_liked']
+
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        try:
+            url = obj.image.url
+        except Exception:
+            return None
+        if request is not None:
+            return request.build_absolute_uri(url)
+        return url
+
+    def get_is_liked(self, obj):
+        request = self.context.get('request')
+        user = getattr(request, 'user', None) if request else None
+        if user and user.is_authenticated:
+            return obj.like_users.filter(pk=user.pk).exists()
+        return False
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,8 +1,29 @@
-from api_v1.serializers.post import PostSerializer
-from webapp.models import Post
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from api_v1.serializers.post import PostSerializer
+from api_v1.permissions import IsAuthorOrReadOnly
+from webapp.models import Post
+
+
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class =  PostSerializer
+    queryset = Post.objects.all().select_related('author').prefetch_related('like_users')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        post.like_users.add(request.user)
+        return Response({'liked': True, 'likes_count': post.like_users.count()})
+
+    @like.mapping.delete
+    def unlike(self, request, pk=None):
+        post = self.get_object()
+        post.like_users.remove(request.user)
+        return Response({'liked': False, 'likes_count': post.like_users.count()})
